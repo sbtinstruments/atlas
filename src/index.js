ebbb18d3c960927773a6d55007144c4c18294a12
tvi <tvi@sbtinstruments.com>
2021-04-20 13:57:58 +0200

    fix: fix icrf rotation and imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 import { Cesium3DTileset, createWorldTerrain, IonResource, Viewer } from 'cesium';
 import "cesium/Build/Cesium/Widgets/widgets.css";
+import SceneMode from 'cesium/Source/Scene/SceneMode';
 import "./css/main.css";
-
+import Transforms from 'cesium/Source/Core/Transforms';
+import Cartesian3 from 'cesium/Source/Core/Cartesian3';
+import Matrix4 from 'cesium/Source/Core/Matrix4';
 // This is simplified version of Cesium's Getting Started tutorial.
 // See https://cesium.com/docs/tutorials/getting-started/ for more details.
 
 var viewer = new Viewer('cesiumContainer');
+
+viewer.scene.mode = SceneMode.SCENE3D
 //////////////////////////////////////////////////////////////////////////
 // Configuring the Scene
 //////////////////////////////////////////////////////////////////////////
@@ -15,28 +20,28 @@ var viewer = new Viewer('cesiumContainer');
 viewer.scene.globe.enableLighting = true;
 
 // Set up 
-// Set up clock and timeline.
-
-viewer.clock.shouldAnimate = true; // default
-viewer.clock.multiplier = 800;
-viewer.scene.primitives.add(tileset);
-viewer.zoomTo(tileset);
-
 function icrf(scene, time) {
-    if (scene.mode !== Cesium.SceneMode.SCENE3D) {
+    if (scene.mode !== SceneMode.SCENE3D) {
         return;
     }
-    var icrfToFixed = Cesium.Transforms.computeIcrfToFixedMatrix(time);
-    if (Cesium.defined(icrfToFixed)) {
+    var icrfToFixed = Transforms.computeIcrfToFixedMatrix(time);
+    if (icrfToFixed) {
         var camera = viewer.camera;
-        var offset = Cesium.Cartesian3.clone(camera.position);
-        var transform = Cesium.Matrix4.fromRotationTranslation(icrfToFixed);
+        var offset = Cartesian3.clone(camera.position);
+        var transform = Matrix4.fromRotationTranslation(icrfToFixed);
         camera.lookAtTransform(transform, offset);
     }
 }
+// // Set up clock and timeline.
+
+// viewer.clock.shouldAnimate = true; // default
+// // viewer.clock.multiplier = 800;
+// // viewer.scene.primitives.add(tileset);
+// // viewer.zoomTo(tileset);
+
 
 function viewInICRF() {        
-    viewer.clock.multiplier = 3 * 60 * 60*100;
+    viewer.clock.multiplier = 60 * 60;
     viewer.scene.postUpdate.addEventListener(icrf);
     viewer.scene.globe.enableLighting = true;
 }
